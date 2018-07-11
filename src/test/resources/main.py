7b1b3222de7fbbe1f4831b2bc96c0aa0bd19927f
Villu Ruusmann <villu.ruusmann@gmail.com>
2018-07-12 01:19:17 +0300

    Updated test resources

--- src/test/resources/main.py
+++ src/test/resources/main.py
@@ -20,6 +20,11 @@ h2o.connect()
 # Binary classification
 #
 
+def load_audit(name):
+	df = load_csv(name)
+	df["Adjusted"] = df["Adjusted"].asfactor()
+	return df
+
 def build_audit(df, classifier, name):
 	classifier.train(*split_columns(df), training_frame = df)
 	store_mojo(classifier, name + ".zip")
@@ -27,19 +32,20 @@ def build_audit(df, classifier, name):
 	adjusted.set_names(["Adjusted", "probability(0)", "probability(1)"])
 	store_csv(adjusted, name + ".csv")
 
-audit_df = load_csv("Audit.csv")
-audit_df["Adjusted"] = audit_df["Adjusted"].asfactor()
+audit_df = load_audit("Audit.csv")
 
 build_audit(audit_df, H2OGeneralizedLinearEstimator(family = "binomial", lambda_ = 0), "GLMAudit")
 
-audit_df = load_csv("AuditNA.csv")
-audit_df["Adjusted"] = audit_df["Adjusted"].asfactor()
+audit_df = load_audit("AuditNA.csv")
 
 build_audit(audit_df, H2OGeneralizedLinearEstimator(family = "binomial"), "GLMAuditNA")
 
 #
 # Multi-class classification
 #
+
+def load_iris(name):
+	return load_csv(name)
 
 def build_iris(df, classifier, name):
 	classifier.train(*split_columns(df), training_frame = df)
@@ -48,13 +54,19 @@ def build_iris(df, classifier, name):
 	species.set_names(["Species", "probability(setosa)", "probability(versicolor)", "probability(virginica)"])
 	store_csv(species, name + ".csv")
 
-iris_df = load_csv("Iris.csv")
+iris_df = load_iris("Iris.csv")
 
 build_iris(iris_df, H2OGeneralizedLinearEstimator(family = "multinomial"), "GLMIris")
 
 #
 # Regression
 #
+
+def load_auto(name):
+	df = load_csv(name)
+	df["cylinders"] = df["cylinders"].asfactor()
+	df["origin"] = df["origin"].asfactor()
+	return df
 
 def build_auto(df, regressor, name):
 	regressor.train(*split_columns(df), training_frame = df)
@@ -63,10 +75,10 @@ def build_auto(df, regressor, name):
 	mpg.set_names(["mpg"])
 	store_csv(mpg, name + ".csv")
 
-auto_df = load_csv("Auto.csv")
+auto_df = load_auto("Auto.csv")
 
 build_auto(auto_df, H2OGeneralizedLinearEstimator(family = "gaussian", lambda_ = 0), "GLMAuto")
 
-auto_df = load_csv("AutoNA.csv")
+auto_df = load_auto("AutoNA.csv")
 
 build_auto(auto_df, H2OGeneralizedLinearEstimator(family = "gaussian"), "GLMAutoNA")
